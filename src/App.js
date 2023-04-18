c7836e1a4335fd50549fd4bfcfc6151bdae7c143
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2023-04-19 00:17:00 +0500

    Working with States

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 10,
+    };
+  }
+
+  clickHandler = () => {
+    console.log("click");
+  };
+
+  render() {
+    const { firstname, lastName, age } = this.props;
+    return (
+      <div className="w-50 mx-auto">
+        <div className="border p-3 mt-2">
+          Hello {firstname} {lastName} {age}
+        </div>
+        <div>
+          <button onClick={this.clickHandler} className="btn btn-primary">
+            Click
+          </button>
+        </div>
+        <div>
+          <p className="text-center">{this.state.counter}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello firstname="Aziz" lastName="Kuchkarov" age={25} />
+      <Hello firstname="Aziz" lastName="Kuchkarov" age={35} />
     </div>
   );
 }
